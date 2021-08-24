5ae5a21afa26e92796cb1875d3ec16e87a385c53
Momo Langenstein <momo.langenstein17@imperial.ac.uk>
2021-08-24 12:42:54 +0000

    (ml5717) Re-added r2cEmbed attribute

--- rust-cuda-derive/src/rust_to_cuda/field_ty.rs
+++ rust-cuda-derive/src/rust_to_cuda/field_ty.rs
@@ -1,17 +1,33 @@
 use syn::{parse_quote, spanned::Spanned};
 
 pub fn swap_field_type_and_filter_attrs(field: &mut syn::Field) -> syn::Type {
-    let cuda_repr_field_ty = field.ty.clone();
+    let mut cuda_repr_field_ty: Option<syn::Type> = None;
+    let old_field_ty = field.ty.clone();
 
-    field.ty = parse_quote! {
-        rust_cuda::common::DeviceAccessible<
-            <#cuda_repr_field_ty as rust_cuda::common::RustToCuda>::CudaRepresentation
-        >
-    };
+    let mut r2c_ignore = false;
 
-    // Remove all field attributes after #[r2cIgnore]
-    if let Some(ignore_from) = field.attrs.iter().position(|attr| {
-        if attr.path.is_ident("r2cIgnore") {
+    // Remove all attributes from the fields in the Cuda representation
+    field.attrs.retain(|attr| {
+        if attr.path.is_ident("r2cEmbed") {
+            if cuda_repr_field_ty.is_none() {
+                if !attr.tokens.is_empty() {
+                    emit_error!(
+                        attr.tokens.span(),
+                        "#[r2cEmbed] does not take any arguments."
+                    );
+                }
+
+                cuda_repr_field_ty = Some(parse_quote! {
+                    rust_cuda::common::DeviceAccessible<
+                        <#old_field_ty as rust_cuda::common::RustToCuda>::CudaRepresentation
+                    >
+                });
+            } else {
+                emit_error!(attr.span(), "Duplicate #[r2cEmbed] attribute definition.");
+            }
+
+            false
+        } else if attr.path.is_ident("r2cIgnore") {
             if !attr.tokens.is_empty() {
                 emit_error!(
                     attr.tokens.span(),
@@ -19,13 +35,23 @@ pub fn swap_field_type_and_filter_attrs(field: &mut syn::Field) -> syn::Type {
                 );
             }
 
-            true
-        } else {
-            false
-        }
-    }) {
-        field.attrs.truncate(ignore_from);
-    }
+            r2c_ignore = true;
 
-    cuda_repr_field_ty
+            false
+        } else {
+            !r2c_ignore
+        }
+    });
+
+    field.ty = if let Some(cuda_repr_field_ty) = cuda_repr_field_ty {
+        cuda_repr_field_ty
+    } else {
+        parse_quote! {
+            rust_cuda::common::DeviceAccessible<
+                rust_cuda::utils::stack::StackOnlyWrapper<#old_field_ty>
+            >
+        }
+    };
+
+    old_field_ty
 }
--- src/utils/stack.rs
+++ src/utils/stack.rs
@@ -2,6 +2,11 @@ use core::ops::{Deref, DerefMut};
 
 use rustacuda_core::DeviceCopy;
 
+use crate::common::{
+    r#impl::{CudaAsRustImpl, RustToCudaImpl},
+    DeviceAccessible,
+};
+
 /// ```rust
 /// # use rust_cuda::utils::stack::StackOnly;
 /// fn assert_stackonly(_x: impl StackOnly) {}
@@ -35,25 +40,25 @@ impl<T: private::StackOnly> StackOnly for T {}
 #[repr(transparent)]
 #[derive(Debug)]
 #[allow(clippy::module_name_repetitions)]
-pub struct StackOnlyDeviceCopy<T: StackOnly>(T);
+pub struct StackOnlyWrapper<T: StackOnly>(T);
 
 // Safety: Any type that is fully on the stack without any references
 //         to the heap can be safely copied to the GPU
-unsafe impl<T: StackOnly> DeviceCopy for StackOnlyDeviceCopy<T> {}
+unsafe impl<T: StackOnly> DeviceCopy for StackOnlyWrapper<T> {}
 
-impl<T: StackOnly> From<T> for StackOnlyDeviceCopy<T> {
+impl<T: StackOnly> From<T> for StackOnlyWrapper<T> {
     fn from(inner: T) -> Self {
         Self(inner)
     }
 }
 
-impl<T: StackOnly> StackOnlyDeviceCopy<T> {
+impl<T: StackOnly> StackOnlyWrapper<T> {
     pub fn into_inner(self) -> T {
         self.0
     }
 }
 
-impl<T: StackOnly> Deref for StackOnlyDeviceCopy<T> {
+impl<T: StackOnly> Deref for StackOnlyWrapper<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -61,9 +66,49 @@ impl<T: StackOnly> Deref for StackOnlyDeviceCopy<T> {
     }
 }
 
-impl<T: StackOnly> DerefMut for StackOnlyDeviceCopy<T> {
+impl<T: StackOnly> DerefMut for StackOnlyWrapper<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
+    }
+}
+
+unsafe impl<T: StackOnly> RustToCudaImpl for StackOnlyWrapper<T> {
+    #[cfg(feature = "host")]
+    type CudaAllocationImpl = crate::host::NullCudaAlloc;
+    type CudaRepresentationImpl = Self;
+
+    #[cfg(feature = "host")]
+    #[allow(clippy::type_complexity)]
+    unsafe fn borrow_impl<A: crate::host::CudaAlloc>(
+        &self,
+        alloc: A,
+    ) -> rustacuda::error::CudaResult<(
+        DeviceAccessible<Self::CudaRepresentationImpl>,
+        crate::host::CombinedCudaAlloc<Self::CudaAllocationImpl, A>,
+    )> {
+        let alloc = crate::host::CombinedCudaAlloc::new(crate::host::NullCudaAlloc, alloc);
+        Ok((DeviceAccessible::from(&self.0), alloc))
+    }
+
+    #[cfg(feature = "host")]
+    #[doc(cfg(feature = "host"))]
+    unsafe fn restore_impl<A: crate::host::CudaAlloc>(
+        &mut self,
+        alloc: crate::host::CombinedCudaAlloc<Self::CudaAllocationImpl, A>,
+    ) -> rustacuda::error::CudaResult<A> {
+        let (_alloc_front, alloc_tail): (crate::host::NullCudaAlloc, A) = alloc.split();
+
+        Ok(alloc_tail)
+    }
+}
+unsafe impl<T: StackOnly> CudaAsRustImpl for StackOnlyWrapper<T> {
+    type RustRepresentationImpl = Self;
+
+    #[cfg(any(not(feature = "host"), doc))]
+    unsafe fn as_rust_impl(this: &DeviceAccessible<Self>) -> Self::RustRepresentationImpl {
+        let mut uninit = core::mem::MaybeUninit::uninit();
+        core::ptr::copy_nonoverlapping(&**this, uninit.as_mut_ptr(), 1);
+        uninit.assume_init()
     }
 }
 
