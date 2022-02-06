5c82d6886b4ba12ffbd7cf88563d3c7d89bb96aa
Momo Langenstein <momo.langenstein@helsinki.fi>
2022-02-06 17:18:47 +0000

    Incremental kernel compilation, but less live output

--- rust-cuda-derive/src/kernel/link/mod.rs
+++ rust-cuda-derive/src/kernel/link/mod.rs
@@ -6,7 +6,7 @@ use std::{
 
 use proc_macro::TokenStream;
 use ptx_builder::{
-    builder::{BuildStatus, Builder, ErrorFormat, Profile},
+    builder::{BuildStatus, Builder, MessageFormat, Profile},
     error::{BuildErrorKind, Error, Result},
     reporter::ErrorLogPrinter,
 };
@@ -161,41 +161,6 @@ fn compile_kernel(
     crate_path: &Path,
     specialisation: Specialisation,
 ) -> String {
-    // Force this proc macro to always be evaluated using a dummy env var
-    env::set_var("RUST_CUDA_DERIVE_MARKER", "42");
-    assert_eq!(
-        proc_macro::tracked_env::var("RUST_CUDA_DERIVE_MARKER").as_deref(),
-        Ok("42")
-    );
-
-    let colourful = if let Some(cargo_colour) = with_cargo_args(|args| {
-        let mut colourful = None;
-
-        for arg in args.windows(2) {
-            colourful = Some(match (arg[0].as_str(), arg[1].as_str()) {
-                ("--color=always", _) | ("--color", "always") | (_, "--color=always") => true,
-                ("--color=never", _) | ("--color", "never") | (_, "--color=never") => false,
-                _ => continue,
-            });
-        }
-
-        colourful
-    }) {
-        cargo_colour
-    } else if let Ok(cargo_colour @ ("always" | "never")) =
-        proc_macro::tracked_env::var("CARGO_TERM_COLOR").as_deref()
-    {
-        cargo_colour == "always"
-    } else if let Ok(term) = proc_macro::tracked_env::var("TERM") {
-        term != "dumb"
-            && matches!(
-                proc_macro::tracked_env::var("NO_COLOR"),
-                Err(env::VarError::NotPresent)
-            )
-    } else {
-        false
-    };
-
     if let Ok(rust_flags) = proc_macro::tracked_env::var("RUSTFLAGS") {
         env::set_var("RUSTFLAGS", rust_flags.replace("-Zinstrument-coverage", ""));
     }
@@ -206,12 +171,7 @@ fn compile_kernel(
         args.to_string().to_uppercase()
     );
 
-    match build_kernel_with_specialisation(
-        crate_path,
-        &specialisation_var,
-        specialisation,
-        colourful,
-    ) {
+    match build_kernel_with_specialisation(crate_path, &specialisation_var, specialisation) {
         Ok(kernel_path) => {
             let mut file = fs::File::open(&kernel_path)
                 .unwrap_or_else(|_| panic!("Failed to open kernel file at {:?}.", &kernel_path));
@@ -224,13 +184,8 @@ fn compile_kernel(
             kernel_ptx
         },
         Err(err) => {
-            let mut error_printer = ErrorLogPrinter::print(err);
-
-            if !colourful {
-                error_printer.disable_colors();
-            }
-
-            abort_call_site!(error_printer);
+            // TODO: Handle colour output here as well - maybe build diagnostic?
+            abort_call_site!(ErrorLogPrinter::print(err));
         },
     }
 }
@@ -239,7 +194,6 @@ fn build_kernel_with_specialisation(
     kernel_path: &Path,
     env_var: &str,
     specialisation: Specialisation,
-    colourful: bool,
 ) -> Result<PathBuf> {
     match specialisation {
         Specialisation::Check => env::set_var(env_var, "chECK"),
@@ -249,38 +203,16 @@ fn build_kernel_with_specialisation(
     let result = (|| {
         let mut builder = Builder::new(kernel_path)?;
 
-        if !colourful {
-            builder = builder.disable_colors();
-        }
-
         builder = match specialisation {
             Specialisation::Check => builder.set_profile(Profile::Debug),
             Specialisation::Link(_) => builder.set_profile(Profile::Release),
         };
 
-        builder = builder.set_error_format(
-            with_cargo_args(|args| {
-                let mut message_format = None;
-
-                for arg in args.windows(2) {
-                    message_format = Some(match (arg[0].as_str(), arg[1].as_str()) {
-                        ("--message-format=human", _)
-                        | ("--message-format", "human")
-                        | (_, "--message-format=human") => ErrorFormat::Human,
-                        ("--message-format=json", _)
-                        | ("--message-format", "json")
-                        | (_, "--message-format=json") => ErrorFormat::JSON,
-                        ("--message-format=short", _)
-                        | ("--message-format", "short")
-                        | (_, "--message-format=short") => ErrorFormat::Short,
-                        _ => continue,
-                    });
-                }
-
-                message_format
-            })
-            .unwrap_or(ptx_builder::builder::ErrorFormat::Human),
-        );
+        builder = builder.set_message_format(MessageFormat::Json {
+            render_diagnostics: false,
+            short: false,
+            ansi: true,
+        });
 
         builder = builder.set_prefix(match specialisation {
             Specialisation::Check => String::from("chECK"),
@@ -289,7 +221,16 @@ fn build_kernel_with_specialisation(
             },
         });
 
-        match builder.build()? {
+        match builder.build_live(
+            |stdout_line| {
+                if let Ok(cargo_metadata::Message::CompilerMessage(message)) =
+                    serde_json::from_str(stdout_line)
+                {
+                    eprintln!("{}", serde_json::to_string(&message.message).unwrap());
+                }
+            },
+            |_line| (),
+        )? {
             BuildStatus::Success(output) => {
                 let ptx_path = output.get_assembly_path();
 
@@ -340,20 +281,4 @@ fn build_kernel_with_specialisation(
 enum Specialisation<'a> {
     Check,
     Link(&'a str),
-}
-
-fn with_cargo_args<Q, F: FnOnce(&[String]) -> Option<Q>>(inner: F) -> Option<Q> {
-    use sysinfo::{ProcessExt, RefreshKind, System, SystemExt};
-
-    let mut system = System::new_with_specifics(RefreshKind::new());
-
-    let pid = sysinfo::get_current_pid().ok()?;
-    system.refresh_process(pid);
-    let process = system.process(pid)?;
-
-    let p_pid = process.parent()?;
-    system.refresh_process(p_pid);
-    let parent = system.process(p_pid)?;
-
-    inner(parent.cmd())
 }
