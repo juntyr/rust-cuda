3e7259b1d1b876de094406c43cd3a6c8c68fec71
Juniper Tyree <juniper.tyree@helsinki.fi>
2024-10-29 18:06:20 +0000

    Ignore some clippy lints with FIXMEs for now

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,10 @@
 //! [Gitpod Ready-to-Code]: https://img.shields.io/badge/Gitpod-ready-blue?logo=gitpod
 //! [gitpod]: https://gitpod.io/#https://github.com/juntyr/rust-cuda
 
+#![allow(missing_docs)] // FIXME
+#![allow(clippy::undocumented_unsafe_blocks)] // FIXME
+#![allow(clippy::multiple_unsafe_ops_per_block)] // FIXME
+#![allow(clippy::indexing_slicing)] // FIXME
 #![cfg_attr(all(any(feature = "device", target_os = "cuda"), not(doc)), no_std)]
 #![feature(auto_traits)]
 #![feature(negative_impls)]
